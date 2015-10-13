d2f919389b78fee931aa4c971f1b971ea6f88ec6
Agronis <agronis@icloud.com>
2015-10-13 08:58:13 -0400

    Homework 2.1 - Final v2

--- src/Time.java
+++ src/Time.java
@@ -6,6 +6,7 @@ import java.util.concurrent.TimeUnit;
 public class Time {
 
     public static void menu() throws InterruptedException {
+
         System.out.println("Returning to the main menu.");
         TimeUnit.MILLISECONDS.sleep(80);
         System.out.println(".");
@@ -23,9 +24,11 @@ public class Time {
         System.out.println(".......");
         TimeUnit.MILLISECONDS.sleep(80);
         System.out.println("........");
+
     }
 
     public static void boot() throws InterruptedException {
+
         System.out.println("System booting.");
         TimeUnit.SECONDS.sleep(1);
         System.out.println(".");
@@ -37,5 +40,21 @@ public class Time {
         System.out.println("....");
         TimeUnit.SECONDS.sleep(1);
         System.out.println(".....");
+
+    }
+
+    public static void fail() throws InterruptedException {
+
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("Four");
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("Three");
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("Two");
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("One");
+        System.out.println("Goodbye.");
+        System.exit(0);
+        
     }
 }
